a49d33494689c795ae8931e5cf76e10f7f7a5da7
BoJackHorseman <1142083668@qq.com>
2017-06-16 17:36:24 +0800

    增加跨浏览器滚轮事件处理程序

--- js/chapter13/event.js
+++ js/chapter13/event.js
@@ -26,6 +26,46 @@ var EventUtil = {
     getTarget: function(event){
         return event.target ? event.target : event.srcElement;
     },
+    //获取相关元素
+    getRelatedTarget: function(event){
+        if(event.relatedTarget){
+            return event.relatedTarget;
+        }else if(event.toElement){
+            return event.toElement;
+        }else if(event.fromElement){
+            return event.fromElement;
+        }else{
+            return null;
+        }
+    },
+    //将IE模型规范化为DOM方式，检测button属性
+    getButton: function(event){
+        if(document.implementation.hasFeature("MouseEvents", "2.0")){
+            return event.button;
+        }else{
+            switch(event.button){
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
+    },
+    //跨浏览器的鼠标滚轮事件
+    getWheelDelta: function(event){
+        if(event.wheelDelta){
+            return event.wheelDelta;
+        }else{
+            return -event.detail * 40;
+        }
+    },
     preventDefault: function(event){
         if(event.preventDefault){
             event.preventDefault();
