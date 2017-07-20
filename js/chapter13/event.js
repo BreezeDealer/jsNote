7a6c9963382c183d036567bc1fcf1d336e3d8b3c
BoJackHorseman <1142083668@qq.com>
2017-07-20 11:23:59 +0800

    增加跨浏览器创建XHR和序列化表单

--- js/chapter13/event.js
+++ js/chapter13/event.js
@@ -101,3 +101,81 @@ var EventUtil = {
         }
     }
 }
+/**
+ * 跨浏览器创建XHR
+ */
+function createXHR() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0", "MSXML2.XMLHttp"],
+                i,
+                len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    console.log(ex);
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    }else {
+        throw new Error("No XHR object available.");
+    }
+}
+//表单序列化
+function serialize(form){
+    var parts = [],
+    field = null,
+    i,
+    len,
+    j,
+    optLen,
+    option,
+    optValue;
+    for(i = 0, len=form.elements.length; i < len; i++){
+        field = form.elements[i];
+        switch(field.type){
+            case "select-one":
+            case "select-multiple":
+
+                if(field.name.length){
+                    for(j=0, optLen = field.options.length; j < optLen; j++){
+                        option = field.options[j];
+                        if(option.selected){
+                            optValue = "";
+                            if(option.hasAttribute){
+                                optValue = (option.hasAttribute("value") ? option.value : option.text);
+                            }else{
+                                optValue = (option.attributes["value"].specified ? option.value : option.text);
+                            }
+                            parts.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(optValue));
+                        }
+                    }
+                }
+            break;
+
+            case undefined:
+            case "file":
+            case "submit":
+            case "reset":
+            case "button":
+                break;
+
+            case "radio":
+            case "checkbox":
+                if(!field.checked){
+                    break;
+                }
+            default:
+                if(field.name.length){
+                    parts.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
+                }
+        }
+    }
+    return parts.join("&");
+}
