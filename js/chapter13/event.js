b326dbb713aeedea6cf928112d45505e7009788d
BoJackHorseman <1142083668@qq.com>
2017-08-04 17:12:34 +0800

    增加拖动和自定义事件

--- js/chapter13/event.js
+++ js/chapter13/event.js
@@ -179,3 +179,102 @@ function serialize(form){
     }
     return parts.join("&");
 }
+//自定义事件
+function EventTarget(){
+    this.handlers = {};
+}
+EventTarget.prototype = {
+    contructor: EventTarget,
+    addHandler: function(type, handler){
+        if(typeof this.handlers[type] == "undefined"){
+            this.handlers[type] = [];
+        }
+        this.handlers[type].push(handler);
+    },
+    fire: function(event){
+        if(!event.target){
+            event.target = this;
+        }
+        if(this.handlers[event.type] instanceof Array){
+            var handlers = this.handlers[event.type];
+            for(var i=0, len = handlers.length; i < len; i++){
+                handlers[i](event);
+            }
+        }
+    },
+    removeHandler: function(type, handler){
+        if(this.handlers[type] instanceof Array){
+            var handlers = this.handlers[type];
+            for(var i=0, len = handlers.length; i < len; i++){
+                if(handlers[i] == handler){
+                    break;
+                }
+            }
+            handlers.splice(i, 1);
+        }
+    }
+}
+//拖放事件
+var DragDrop = function(){
+    var dragdrop = new EventTarget(),
+        dragging = null,
+        diffX = 0,
+        diffY = 0;
+    function handleEvent(event){
+        //获取事件和目标
+        event = EventUtil.getEvent(event);
+        var target = EventUtil.getTarget(event);
+        //确定事件类型
+        switch(event.type){
+            case "mousedown":
+                if(target.className.indexOf("draggable") > -1){
+                    dragging = target;
+                    diffX = event.clientX - target.offsetLeft;
+                    diffY = event.clientY - target.offsetTop;
+                    dragdrop.fire({
+                        type:"dragstart", 
+                        target: dragging, 
+                        x: event.clientX, 
+                        y: event.clientY
+                    });
+                }
+                break;
+            case "mousemove":
+                if(dragging !== null){
+                    //指定位置
+                    dragging.style.left = (event.clientX - diffX) + "px";
+                    dragging.style.top = (event.clientY - diffY) + "px";
+                    //触发自定义事件
+                    dragdrop.fire({
+                        type:"drag", 
+                        target: dragging, 
+                        x: event.clientX, 
+                        y: event.clientY
+                    });
+                }
+                break;
+            case "mouseup":
+                dragdrop.fire({
+                    type:"dragend", 
+                    target: dragging, 
+                    x: event.clientX, 
+                    y: event.clientY
+                });
+                dragging = null;
+                break;
+        }
+    };
+    //公共接口
+    dragdrop.enable = function(){
+        EventUtil.addHandler(document, "mousedown", handleEvent);
+        EventUtil.addHandler(document, "mousemove", handleEvent);
+        EventUtil.addHandler(document, "mouseup", handleEvent);            
+    },
+    dragdrop.disable = function(){
+        EventUtil.removeHandler(document, "mousedown", handleEvent);
+        EventUtil.removeHandler(document, "mousemove", handleEvent);
+        EventUtil.removeHandler(document, "mouseup", handleEvent);            
+    }
+    return dragdrop;
+
+}();
