034a67b7aa52e4cf2ed6a3da0c5c32a155547db4
BoJackHorseman <1142083668@qq.com>
2017-08-08 17:54:00 +0800

    增加设置子Cookie方法

--- js/chapter13/event.js
+++ js/chapter13/event.js
@@ -2,48 +2,48 @@
  * 跨浏览器的事件处理程序和事件对象
  */
 var EventUtil = {
-    addHandler: function(ele, type, handler){
-        if(ele.addEventListener){
+    addHandler: function (ele, type, handler) {
+        if (ele.addEventListener) {
             ele.addEventListener(type, handler, false);
-        }else if(ele.attachEvent){
+        } else if (ele.attachEvent) {
             ele.attachEvent("on" + type, handler);
-        }else{
+        } else {
             ele["on" + type] = handler;
         }
     },
-    removeHandler: function(ele, type, handler){
-        if(ele.removeEventListener){
+    removeHandler: function (ele, type, handler) {
+        if (ele.removeEventListener) {
             ele.removeEventListener(type, handler, false);
-        }else if(ele.detachEvent){
+        } else if (ele.detachEvent) {
             ele.detachEvent("on" + type, handler);
-        }else{
+        } else {
             ele["on" + type] = null;
         }
     },
-    getEvent: function(event){
+    getEvent: function (event) {
         return event ? event : window.event;
     },
-    getTarget: function(event){
+    getTarget: function (event) {
         return event.target ? event.target : event.srcElement;
     },
     //获取相关元素
-    getRelatedTarget: function(event){
-        if(event.relatedTarget){
+    getRelatedTarget: function (event) {
+        if (event.relatedTarget) {
             return event.relatedTarget;
-        }else if(event.toElement){
+        } else if (event.toElement) {
             return event.toElement;
-        }else if(event.fromElement){
+        } else if (event.fromElement) {
             return event.fromElement;
-        }else{
+        } else {
             return null;
         }
     },
     //将IE模型规范化为DOM方式，检测button属性
-    getButton: function(event){
-        if(document.implementation.hasFeature("MouseEvents", "2.0")){
+    getButton: function (event) {
+        if (document.implementation.hasFeature("MouseEvents", "2.0")) {
             return event.button;
-        }else{
-            switch(event.button){
+        } else {
+            switch (event.button) {
                 case 0:
                 case 1:
                 case 3:
@@ -59,44 +59,44 @@ var EventUtil = {
         }
     },
     //跨浏览器取得keypress的字符编码
-    getCharCode: function(event){
-        if(typeof event.charCode == "number"){
+    getCharCode: function (event) {
+        if (typeof event.charCode == "number") {
             return event.charCode;
-        }else{
+        } else {
             return event.keyCode;
         }
     },
     //跨浏览器操作剪贴板
-    getClipboardText: function(event){
+    getClipboardText: function (event) {
         var clipboardData = (event.clipboardData || window.clipboardData);
         return clipboardData.getData("text");
     },
-    setClipboardText: function(event, value){
-        if(event.clipboardData){
+    setClipboardText: function (event, value) {
+        if (event.clipboardData) {
             return even.clipboardData.setData("text/plain", value);
-        }else if(window.clipboardData){
+        } else if (window.clipboardData) {
             return window.clipboardData.setData("text", value);
         }
     },
     //跨浏览器的鼠标滚轮事件
-    getWheelDelta: function(event){
-        if(event.wheelDelta){
+    getWheelDelta: function (event) {
+        if (event.wheelDelta) {
             return event.wheelDelta;
-        }else{
+        } else {
             return -event.detail * 40;
         }
     },
-    preventDefault: function(event){
-        if(event.preventDefault){
+    preventDefault: function (event) {
+        if (event.preventDefault) {
             event.preventDefault();
-        }else{
+        } else {
             event.returnValue = false;
         }
     },
-    stopPropagation: function(event){
-        if(event.stopPropagation){
+    stopPropagation: function (event) {
+        if (event.stopPropagation) {
             event.stopPropagation();
-        }else{
+        } else {
             event.cancelBubble = true;
         }
     }
@@ -123,41 +123,41 @@ function createXHR() {
             }
         }
         return new ActiveXObject(arguments.callee.activeXString);
-    }else {
+    } else {
         throw new Error("No XHR object available.");
     }
 }
 //表单序列化
-function serialize(form){
+function serialize(form) {
     var parts = [],
-    field = null,
-    i,
-    len,
-    j,
-    optLen,
-    option,
-    optValue;
-    for(i = 0, len=form.elements.length; i < len; i++){
+        field = null,
+        i,
+        len,
+        j,
+        optLen,
+        option,
+        optValue;
+    for (i = 0, len = form.elements.length; i < len; i++) {
         field = form.elements[i];
-        switch(field.type){
+        switch (field.type) {
             case "select-one":
             case "select-multiple":
 
-                if(field.name.length){
-                    for(j=0, optLen = field.options.length; j < optLen; j++){
+                if (field.name.length) {
+                    for (j = 0, optLen = field.options.length; j < optLen; j++) {
                         option = field.options[j];
-                        if(option.selected){
+                        if (option.selected) {
                             optValue = "";
-                            if(option.hasAttribute){
+                            if (option.hasAttribute) {
                                 optValue = (option.hasAttribute("value") ? option.value : option.text);
-                            }else{
+                            } else {
                                 optValue = (option.attributes["value"].specified ? option.value : option.text);
                             }
                             parts.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(optValue));
                         }
                     }
                 }
-            break;
+                break;
 
             case undefined:
             case "file":
@@ -168,11 +168,11 @@ function serialize(form){
 
             case "radio":
             case "checkbox":
-                if(!field.checked){
+                if (!field.checked) {
                     break;
                 }
             default:
-                if(field.name.length){
+                if (field.name.length) {
                     parts.push(encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value));
                 }
         }
@@ -180,33 +180,33 @@ function serialize(form){
     return parts.join("&");
 }
 //自定义事件
-function EventTarget(){
+function EventTarget() {
     this.handlers = {};
 }
 EventTarget.prototype = {
     contructor: EventTarget,
-    addHandler: function(type, handler){
-        if(typeof this.handlers[type] == "undefined"){
+    addHandler: function (type, handler) {
+        if (typeof this.handlers[type] == "undefined") {
             this.handlers[type] = [];
         }
         this.handlers[type].push(handler);
     },
-    fire: function(event){
-        if(!event.target){
+    fire: function (event) {
+        if (!event.target) {
             event.target = this;
         }
-        if(this.handlers[event.type] instanceof Array){
+        if (this.handlers[event.type] instanceof Array) {
             var handlers = this.handlers[event.type];
-            for(var i=0, len = handlers.length; i < len; i++){
+            for (var i = 0, len = handlers.length; i < len; i++) {
                 handlers[i](event);
             }
         }
     },
-    removeHandler: function(type, handler){
-        if(this.handlers[type] instanceof Array){
+    removeHandler: function (type, handler) {
+        if (this.handlers[type] instanceof Array) {
             var handlers = this.handlers[type];
-            for(var i=0, len = handlers.length; i < len; i++){
-                if(handlers[i] == handler){
+            for (var i = 0, len = handlers.length; i < len; i++) {
+                if (handlers[i] == handler) {
                     break;
                 }
             }
@@ -215,49 +215,50 @@ EventTarget.prototype = {
     }
 }
 //拖放事件
-var DragDrop = function(){
+var DragDrop = function () {
     var dragdrop = new EventTarget(),
         dragging = null,
         diffX = 0,
         diffY = 0;
-    function handleEvent(event){
+
+    function handleEvent(event) {
         //获取事件和目标
         event = EventUtil.getEvent(event);
         var target = EventUtil.getTarget(event);
         //确定事件类型
-        switch(event.type){
+        switch (event.type) {
             case "mousedown":
-                if(target.className.indexOf("draggable") > -1){
+                if (target.className.indexOf("draggable") > -1) {
                     dragging = target;
                     diffX = event.clientX - target.offsetLeft;
                     diffY = event.clientY - target.offsetTop;
                     dragdrop.fire({
-                        type:"dragstart", 
-                        target: dragging, 
-                        x: event.clientX, 
+                        type: "dragstart",
+                        target: dragging,
+                        x: event.clientX,
                         y: event.clientY
                     });
                 }
                 break;
             case "mousemove":
-                if(dragging !== null){
+                if (dragging !== null) {
                     //指定位置
                     dragging.style.left = (event.clientX - diffX) + "px";
                     dragging.style.top = (event.clientY - diffY) + "px";
                     //触发自定义事件
                     dragdrop.fire({
-                        type:"drag", 
-                        target: dragging, 
-                        x: event.clientX, 
+                        type: "drag",
+                        target: dragging,
+                        x: event.clientX,
                         y: event.clientY
                     });
                 }
                 break;
             case "mouseup":
                 dragdrop.fire({
-                    type:"dragend", 
-                    target: dragging, 
-                    x: event.clientX, 
+                    type: "dragend",
+                    target: dragging,
+                    x: event.clientX,
                     y: event.clientY
                 });
                 dragging = null;
@@ -265,50 +266,130 @@ var DragDrop = function(){
         }
     };
     //公共接口
-    dragdrop.enable = function(){
-        EventUtil.addHandler(document, "mousedown", handleEvent);
-        EventUtil.addHandler(document, "mousemove", handleEvent);
-        EventUtil.addHandler(document, "mouseup", handleEvent);            
-    },
-    dragdrop.disable = function(){
-        EventUtil.removeHandler(document, "mousedown", handleEvent);
-        EventUtil.removeHandler(document, "mousemove", handleEvent);
-        EventUtil.removeHandler(document, "mouseup", handleEvent);            
-    }
+    dragdrop.enable = function () {
+            EventUtil.addHandler(document, "mousedown", handleEvent);
+            EventUtil.addHandler(document, "mousemove", handleEvent);
+            EventUtil.addHandler(document, "mouseup", handleEvent);
+        },
+        dragdrop.disable = function () {
+            EventUtil.removeHandler(document, "mousedown", handleEvent);
+            EventUtil.removeHandler(document, "mousemove", handleEvent);
+            EventUtil.removeHandler(document, "mouseup", handleEvent);
+        }
     return dragdrop;
 
 }();
 //读写cookie
 var CookieUtil = {
-    get: function(name){
+    get: function (name) {
         var cookieName = encodeURIComponent(name) + "=",
             cookieStart = document.cookie.indexOf(cookieName),
             cookieValue = null;
-        if(cookieStart < -1){
+        if (cookieStart > -1) {
             var cookieEnd = document.cookie.indexOf(";", cookieStart);
-            if(cookieEnd == -1){
+            if (cookieEnd == -1) {
                 cookieEnd = document.cookie.length;
             }
             cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
         }
+        return cookieValue;
     },
-    set: function(name, value, expires, path, domain, secure){
+    set: function (name, value, expires, path, domain, secure) {
         var cookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-        if(expires instanceof Date){
+        if (expires instanceof Date) {
             cookieText += "; expires=" + expires.toGMTString();
         }
-        if(path){
+        if (path) {
             cookieText += "; path=" + path;
         }
-        if(domain){
+        if (domain) {
             cookieText += "; domain" + domain;
         }
-        if(secure){
+        if (secure) {
             cookieText += "; secure";
         }
         document.cookie = cookieText;
     },
-    unset: function(name, path, domain, secure){
+    unset: function (name, path, domain, secure) {
         this.set(name, "", new Date(0), path, domain, secure);
+    }
+}
+//设置子cookie
+var SubCookieUtil = {
+    get: function (name, subName) {
+        var subCookies = this.getAll(name);
+        if (subCookies) {
+            return subCookies[subName];
+        } else {
+            return null;
+        }
+    },
+    getAll: function (name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null,
+            subCookies,
+            i,
+            parts,
+            result = {};
+        if (cookieStart > -1) {
+            cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = document.cookie.substring(cookieStart + cookieName.length, cookieEnd);
+            if (cookieValue.length > 0) {
+                subCookies = cookieValue.split("&");
+                for (i = 0, len = subCookies.length; i < len; i++) {
+                    parts = subCookies[i].split("=");
+                    result[decodeURIComponent(parts[0])] = decodeURIComponent(parts[1]);
+                }
+                return result;
+            }
+        }
+        return null;
+    },
+    set: function (name, subName, value, expires, path, domain, secure) {
+        var subCookies = this.getAll(name) || {};
+        subCookies[subName] = value;
+        this.setAll(name, subCookies, expires, path, domain, secure);
+    },
+    setAll: function (name, subCookies, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=",
+            subCookiesParts = new Array(),
+            subName;
+        for (subName in subCookies) {
+            if (subName.length > 0 && subCookies.hasOwnProperty(subName)) {
+                subCookiesParts.push(encodeURIComponent(subName) + "=" + encodeURIComponent(subCookies[subName]));
+            }
+        }
+        if (subCookiesParts.length > 0) {
+            cookieText += subCookiesParts.join("&");
+            if (expires instanceof Date) {
+                cookieText += "; expires=" + expires.toGMTString();
+            }
+            if (path) {
+                cookieText += "; path=" + path;
+            }
+            if (domain) {
+                cookieText += "; domain=" + domain;
+            }
+            if (secure) {
+                cookieText += "; secure";
+            }
+        } else {
+            cookieText += "; expires=" + (new Date(0)).toGMTString();
+        }
+        document.cookie = cookieText;
+    },
+    unset: function(name, subName, path, domain, secure){
+        var subCookies = this.getAll(name);
+        if(subCookies){
+            delete subCookies[subName];
+            this.setAll(name, subCookies, null, path, domain, secure);
+        }
+    },
+    unsetAll: function(name, path, domain, secure){
+        this.setAll(name, null, new Date(0), path, domain, secure);
     }
 }
