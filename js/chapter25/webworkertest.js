b271024e3990240e584cdde69d6ef959c86cc45c
BoJackHorseman <1142083668@qq.com>
2017-08-25 16:15:55 +0800

    Web Worker作用域

---
+++ js/chapter25/webworkertest.js
@@ -0,0 +1,10 @@
+﻿self.onmessage = e =>{
+    var data = e.data;
+    data.sort((a, b) =>{
+        return a - b;
+    });
+    self.postMessage(data);
+}
+console.log(self);
+console.log("this is required js file.");
+importScripts("test.js");
